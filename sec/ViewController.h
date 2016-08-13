121eed75fb5404ae090302c00a5868f5430d1539
刘中天 <liuzhongtian@192.168.0.103>
2016-08-13 09:03:01 +0800

    Initial Commit

---
+++ sec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sec
+//
+//  Created by 刘中天 on 16/8/13.
+//  Copyright © 2016年 niubaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sec/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sec
+//
+//  Created by 刘中天 on 16/8/13.
+//  Copyright © 2016年 niubaba. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
